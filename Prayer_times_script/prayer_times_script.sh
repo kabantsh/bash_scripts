b31fd02621a27ea5d176703a9b6a0862ffbb6da2
Mohamed Ayman <kabantsh@gmail.com>
2024-03-31 08:25:57 +0400

    after adding prayer_times_script.sh file

---
+++ Prayer_times_script/prayer_times_script.sh
@@ -0,0 +1,13 @@
+
+PRAYER_URL="https://api.aladhan.com/v1/timingsByAddress/31-03-2024?address=Dubai,UAE&method=8"
+MY_PRAYER_OUTPUT_FILE="MY_PRAYER_OUTPUT"
+wget https://api.aladhan.com/v1/timingsByAddress/31-03-2024?address=Dubai,UAE&method=8 -o $MY_PRAYER_OUTPUT_FILE
+#wget "$PRAYER_URL"
+
+
+
+# Reference URL
+# !!!!!!!!!!!!!!¡¡!!!!!!!!!!!!!!!!!!!!!
+#https://aladhan.com/prayer-times-api
+# https://api.aladhan.com/v1/timingsByAddress/31-03-2024?address=Dubai,UAE&method=8
+# !!!!!!!!!!!!!!¡¡!!!!!!!!!!!!!!!!!!!!!
