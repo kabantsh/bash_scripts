093c797910b60b34c513a76cab3b03113f6dd1bf
Mohamed Ayman <kabantsh@gmail.com>
2024-04-01 19:54:31 +0400

    after adding restsh tool to prayer_times_script.sh

--- Prayer_times_script/prayer_times_script.sh
+++ Prayer_times_script/prayer_times_script.sh
@@ -1,12 +1,35 @@
+# This script is been developed by kabantsh on 2024-03-31   
+# for more information please contact with the email kabantsh@gmail.com
+################
+# Prerequisits:-
+################
+#1- Operating System
+#------------------- 
+# Linux or Android Termux
+#
+#2- Bash version                                                                
+#------------------
+#5.0 or higher
+#
+#3- Packages version
+#-----------------------
+#- Curl version (8.0 or higher)
+#- Wget version (1.2 or higher)
+##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+set -x
 PRAYER_URL="https://api.aladhan.com/v1/timingsByAddress/31-03-2024?address=Dubai,UAE&method=8"
 #PWD="`PWD`"
 MY_PRAYER_OUTPUT_DIR="`pwd`/output_prayer_times"
-mkdir -p $MY_PRAYER_OUTPUT_FILE
-MY_PRAYER_OUTPUT_FILE="`printf $`MY_PRAYER_OUTPUT_DIR/prayer_times_script.log"
+> "$MY_PRAYER_OUTPUT_DIR"
+mkdir -p $MY_PRAYER_OUTPUT_DIR
+MY_PRAYER_OUTPUT_FILE="`printf `$MY_PRAYER_OUTPUT_DIR/prayer_times_script.log"
 
-wget -o $MY_PRAYER_OUTPUT_FILE "https://api.aladhan.com/v1/timingsByAddress/31-03-2024?address=Dubai,UAE&method=8"
+#wget -o $MY_PRAYER_OUTPUT_FILE "https://api.aladhan.com/v1/timingsByAddress/31-03-2024?address=Dubai,UAE&method=8"
+#curl -o $MY_PRAYER_OUTPUT_FILE "https://api.aladhan.com/v1/timingsByAddress/31-03-2024?address=Dubai,UAE&method=8" 
+restish get "https://api.aladhan.com/v1/timingsByAddress/31-03-2024?address=Dubai,UAE&method=8" | tee -a "$MY_PRAYER_OUTPUT_FILE"
 #wget "$PRAYER_URL"
+set +x
 
 
 
